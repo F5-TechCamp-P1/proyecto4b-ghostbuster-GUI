b01d6cd5e2482e99ea09d3402806cebba1779fc8
Ayuik <marcosayelen@gmail.com>
2025-02-13 17:21:09 +0100

    GBMainFrame visible. WIP

--- src/main/java/dev/cachaguercus/proyecto4/CachaguercusApp.java
+++ src/main/java/dev/cachaguercus/proyecto4/CachaguercusApp.java
@@ -5,8 +5,12 @@ import javax.swing.*;
 import dev.cachaguercus.proyecto4.views.GBMainFrame;
 
 public class CachaguercusApp {
-public static void main(String[] args) {
-    GBMainFrame gbMainFrame = new GBMainFrame();
-    gbMainFrame.initComponents();
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                GBMainFrame mainframe = new GBMainFrame();
+            }
+        });
     }
 }
+
--- src/main/java/dev/cachaguercus/proyecto4/views/GBMainFrame.java
+++ src/main/java/dev/cachaguercus/proyecto4/views/GBMainFrame.java
@@ -5,18 +5,47 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import dev.cachaguercus.proyecto4.controllers.GhostBusterController;
-import dev.cachaguercus.proyecto4.models.GhostBusterModel;
-
 public class GBMainFrame extends JFrame {
-
     public GBMainFrame() {
-        initComponents();
-    }
-
-    public void initComponents() {
-        setSize(1000, 1500);
         setTitle("Cachagüercus Asturias");
+        setSize(1000, 1500);
+        setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+
+        JLabel title = new JLabel("Ghost Busters Asturias");
+        title.setFont(new Font("Showcard Gothic", Font.BOLD, 24));
+        title.setAlignmentX(CENTER_ALIGNMENT);
+
+        String name = "";
+
+        JLabel messageToPlayer = new JLabel("Bienvenido " + name);
+        messageToPlayer.setAlignmentX(CENTER_ALIGNMENT);
+
+        JLabel gbNameRequest = new JLabel("Introduce tu nombre:");
+        JTextField gbNameInput = new JTextField(20);
+        gbNameInput.setMaximumSize(new Dimension(200, 25));
+        JButton button = new JButton("Comenzar juego");
+
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String name = gbNameInput.getText();
+                messageToPlayer.setText("Bienvenido " + name);
+            }
+        });
+
+        add(title);
+        add(messageToPlayer);
+        add(Box.createVerticalStrut(10));
+
+        JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(new BoxLayout(inputPanel, BoxLayout.X_AXIS));
+        inputPanel.setAlignmentX(CENTER_ALIGNMENT);
+        inputPanel.add(gbNameRequest);
+        inputPanel.add(gbNameInput);
+        inputPanel.add(button);
+
+        add(inputPanel);
+
         setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
